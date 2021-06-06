6453bb4d32e8fdb12d9335eb43b3ff357e05b27b
Dariana <darianitamartinezjimenez17@gmail.com>
2021-06-06 02:31:19 -0400

    Header realizado

--- app/Views/comun/header.php
+++ app/Views/comun/header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
-
+<meta charset= "utf8">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,12 +8,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <title>App Radio Taxi</title>
+    <nav class="navbar navbar-expand-md navbar-light" style="background-color: #393E46;">
+        <div class = "container-fluid" style = "height : 45px">
+            <div class="container-fluid bg-light col-10 rounded-pill">
+            <h5 class = "text-muted">www.uber-camba.com</h5>
+        </div>
+        <div class="col-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="21.333" viewBox="0 0 19 21.333">
+             <path id="update" d="M17.517,8a8.953,8.953,0,0,1,6.545,2.954l1.368-1.536a.911.911,0,0,1,1.569.73v5.766a.981.981,0,0,1-.919,1.032H20.945a1.052,1.052,0,0,1-.65-1.762l1.6-1.8a6.034,6.034,0,0,0-4.337-1.947c-3.539-.034-6.523,3.182-6.492,7.288.029,3.9,2.841,7.164,6.435,7.164A6,6,0,0,0,21.738,24.1a.423.423,0,0,1,.627.024l1.519,1.706a.562.562,0,0,1-.018.75A8.914,8.914,0,0,1,17.5,29.333c-5.247,0-9.5-4.776-9.5-10.667S12.277,7.989,17.517,8Z" transform="translate(-8 -8)" fill="#fbfbfb"/>
+            </svg>
+        </div>
+        </div>
+    </nav>
 </head>
 
 <body>
-    <div>
-        <h3>
-            Encabezado
-            <small class="text-muted">App Radio movil</small>
-        </h3>
-    </div>
+</body>
