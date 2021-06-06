eae04f70ed6461100e452f00ba054ca1ce2ccc3f
jenifferbalcazar <jenifferbalcazarjustiniano@gmail.com>
2021-06-06 17:16:09 -0400

    Se añadió la vista del mapa

--- app/Views/comun/header.php
+++ app/Views/comun/header.php
@@ -10,14 +10,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <title>App Radio Taxi</title>
 
-        <div class = "container-fluid" style = "height : 45px">
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="21" viewBox="0 0 30 21">
-            <g id="Icon_feather-menu" data-name="Icon feather-menu" transform="translate(-3 -7.5)">
-                <path id="Trazado_61" data-name="Trazado 61" d="M4.5,18h27" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-                <path id="Trazado_62" data-name="Trazado 62" d="M4.5,9h27" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-                <path id="Trazado_63" data-name="Trazado 63" d="M4.5,27h27" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-            </g>
-        </svg>
+    <nav class="navbar navbar-fixed-bottom " style="background-color: black">
+        <div class = "container-fluid" style = "height : 45px" >
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="21" viewBox="0 0 30 21">
+                <g id="Icon_feather-menu" data-name="Icon feather-menu" transform="translate(-3 -7.5)">
+                    <path id="Trazado_61" data-name="Trazado 61" d="M4.5,18h27" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+                    <path id="Trazado_62" data-name="Trazado 62" d="M4.5,9h27" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+                    <path id="Trazado_63" data-name="Trazado 63" d="M4.5,27h27" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+                </g>
+            </svg>
         </div>
     </nav>
 </head>
