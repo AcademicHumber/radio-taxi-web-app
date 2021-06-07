df687743173573738e6e7de1daf7fd5cf7798aa1
Dariana <darianitamartinezjimenez17@gmail.com>
2021-06-06 23:54:58 -0400

    Cliente Diseños

--- app/Views/mapa/mapa.php
+++ app/Views/mapa/mapa.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-sm-12 border-2">
+    <div class="col-sm-12">
     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d46173.3899904794!2d-63.182228394821024!3d-17.78715264721985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sbo!4v1623010461035!5m2!1ses!2sbo" width="390" height="400" style="border:0;" allowfullscreen="" loading="lazy" class="mx-auto d-block"></iframe> 
     </div>
 </div>
